0ffa465335207085875baa427b460b0b9d73b872
Сергей Жарковский <sergeizharkovsky@yandex.ru>
2022-08-27 08:07:02 +0300

    DEBUG

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='1234567')
 
-DEBUG = os.getenv('DEBUG', default='False') == 'False'
+# DEBUG = os.getenv('DEBUG', default='False') == 'False'
+
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
